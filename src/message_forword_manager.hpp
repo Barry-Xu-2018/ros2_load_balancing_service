3bb71ce3f858fbe8c11a8441d3e5f0831ea66f70
Barry Xu <barry.xu@sony.com>
2024-09-04 17:31:56 +0800

    Add MessageForwardManager class
    Signed-off-by: Barry Xu <barry.xu@sony.com>

---
+++ src/message_forword_manager.hpp
@@ -0,0 +1,27 @@
+// Copyright 2024 Sony Group Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MESSAGE_FORWARD_MANAGER_HPP_
+#define MESSAGE_FORWARD_MANAGER_HPP_
+
+#include "data_queues.hpp"
+#include "load_balancing_policy.hpp"
+#include "service_client_proxy_manager.hpp"
+#include "service_server_proxy.hpp"
+
+class MessageForwardManager{
+
+};
+
+#endif  // MESSAGE_FORWARD_MANAGER_HPP_
